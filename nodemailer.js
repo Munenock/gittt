db341200f34657f3837902b696d37e5fb31a6e8a
Munenock <118257358+Munenock@users.noreply.github.com>
2025-01-03 17:36:22 +0300

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -13,7 +13,7 @@ var mailOptions = {
   from: 'wherea00@gmail.com',
   to: 'wherea00@gmail.com',
   subject: 'testing email',
-  text: 'That was easy trying out the git shit!'
+  text: 'That was easy'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
